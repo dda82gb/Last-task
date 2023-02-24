ff87b79cf3389fc7529b49f064c5110c24fb36e3
Davran_Djumanov <dda_82@mail.ru>
2023-02-25 00:51:09 +0600

    add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Blue;
+int m = Input("Введите M: ");
+int n = Input("Введите N: ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"Сумма элементов от {m} до {n} = {CountNatSum(m, n)}");
+
+int Input(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int CountNatSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNatSum(m, n - 1);
+}
